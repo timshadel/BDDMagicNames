ae5c6211bcef06b614de2bc70168f6f4d262386e
Tim Shadel <tim@shadelsoftware.com>
2017-04-17 12:14:23 -0600

    Initial Commit

---
+++ BDDMagicNames/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BDDMagicNames
+//
+//  Created by Tim on 4/17/17.
+//  Copyright © 2017 Tim Shadel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
